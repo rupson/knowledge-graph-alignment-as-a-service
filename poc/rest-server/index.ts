6a0231a6a18c87858be4c1efa57094067d65cae4
Rob Upson <44114899+rupson@users.noreply.github.com>
2022-04-15 22:21:19 +0100

    functioning proof of concept. still requires error handling

--- poc/rest-server/index.ts
+++ poc/rest-server/index.ts
@@ -1,18 +1,50 @@
-import express from 'express'
-import child_process from 'child_process'
+import express from "express";
+import child_process from "child_process";
+import util from "util";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/double', (req, res) => {
-  const num = req.query.num
+// class OutputWrapper {
+// 	public value: string;
 
-  child_process.execSync(
-    `python ./index ${num}`
-)
-  res.status(200).send({result: 'x'})
-})
+// 	constructor() {
+// 		this.value = "";
+// 	}
+
+// 	public captureCommandOutput: Parameters<typeof child_process.exec>[2] = (
+// 		error,
+// 		stdout,
+// 		stdin,
+// 	) => {
+// 		typeof stdout === "string"
+// 			? (this.value = stdout.trim())
+// 			: (this.value = "fuck");
+// 	};
+// }
+
+const exec = util.promisify(child_process.exec);
+
+// const captureCommandOutput: Parameters<typeof child_process.exec>[2] = (
+// 	error,
+// 	stdout,
+// 	stdin,
+// ) => (typeof stdout === "string" ? stdout.trim() : null);
+
+app.get("/double", async (req, res) => {
+	const num = req.query.num;
+
+	// const outputBucket = new OutputWrapper();
+
+	const result = await exec(
+		`ssh -o StrictHostKeyChecking=no rob@doubler "python ./app ${num}"`,
+		{},
+		// outputBucket.captureCommandOutput,
+	).then(({ stdout }) => Number(stdout.trim()));
+
+	return res.status(200).send({ result });
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}...`)
-})
+	console.log(`listening on port ${port}...`);
+});
