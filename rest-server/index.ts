7ad73f8d0aa6b4591e8911341d84f7bbfce663b9
Rob Upson <44114899+rupson@users.noreply.github.com>
2022-05-16 01:31:46 +0100

    fix server config

--- rest-server/index.ts
+++ rest-server/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import { execInSsh } from "./sshHelpers";
+
+const JAVA_BINARY = `/opt/java/openjdk/bin/java`
 
 const ensureIsDefined = (
 	maybeUndefined: Array<unknown>,
@@ -24,11 +28,17 @@ ensureIsDefined([logmapUrl, sshUser], {
 const execInLogmap = execInSsh(sshUser as string, logmapUrl as string);
 
 const app = express();
-const port = 3000;
+const port = 4000;
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/ping', (req, res) => res.send('pong\n'))
 
 app.post("/align", async (req, res) => {
+	console.log(`>>>> received req`)
 	await execInLogmap(
-		`java -jar target/logmap-matcher-4.0.jar MATCHER file:/usr/src/app/data/human.owl file:/usr/src/app/data/mouse.owl /usr/src/app/out/ true`,
+		`${JAVA_BINARY} -jar target/logmap-matcher-4.0.jar MATCHER file:/usr/src/app/data/human.owl file:/usr/src/app/data/mouse.owl /usr/src/app/out/ true`,
 	);
 	console.log(`>> alignment complete >>`);
 	const result = await execInLogmap(`cat ../out/logmap2_mappings.txt`);
