09360845b10655da76705384253a5ff3be34a461
Rob Upson <44114899+rupson@users.noreply.github.com>
2022-06-20 22:14:42 +0100

    Terraform storage for alignment outputs

---
+++ infrastructure/alignment_outputs.tf
@@ -0,0 +1,14 @@
+resource "azurerm_storage_account" "alignment_outputs" {
+  name                     = "kgaaasalignmentoutputs"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
+}
+
+resource "azurerm_storage_container" "alignment_outputs" {
+  name                  = "outputs"
+  storage_account_name  = azurerm_storage_account.alignment_outputs.name
+  container_access_type = "blob"
+}
